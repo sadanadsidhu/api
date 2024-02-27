ef88fa0ebdeada90b363e21213b675c4a78a4ad1
sadanand kumar <sadanandsidhu@gmail.com>
2024-02-27 20:34:25 +0530

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,91 +1,9 @@
-// const User = require("../models/user");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// const jwtSecretKey = "sadanand"; // Replace with your actual secret key
 
-// const test = (req, res) => {
-//   res.json("test is working");
-// };
-
-// ////////////////////////signup
-// const registerUser = async (req, res) => {
-//   try {
-//     const { username, email, password, role } = req.body;
-//     // Check if name was entered
-//     if (!username) {
-//       return res.json({
-//         error: "name is required",
-//       });
-//     }
-//     // Check password
-//     if (!password || password.length < 6) {
-//       return res.json({
-//         error: "password is required and must be at least 6 characters long",
-//       });
-//     }
-//     // Check email
-//     const exists = await User.findOne({ email });
-//     if (exists) {
-//       return res.json({
-//         error: "Email already exists. Please try a new email",
-//       });
-//     }
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = await User.create({
-//       username,
-//       email,
-//       password: hashedPassword,
-//       role,
-//     });
-//     return res.json(user);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
-// ///////////////////////login
-// const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Find the user by email
-//     const user = await User.findOne({ email });
-
-//     // If user is not found
-//     if (!user) {
-//       return res.status(404).json({ error: "user already exist" });
-//     }
-
-//     // Compare passwords
-//     const passwordMatch = await bcrypt.compare(password, user.password); // Change to user.password
-
-//     // If passwords don't match
-//     if (!passwordMatch) {
-//       return res.status(401).json({ error: "Invalid password" });
-//     }
-
-//     // Generate JWT token
-//     const token = jwt.sign({ userId: user._id }, jwtSecretKey, {
-//       expiresIn: "1h",
-//     });
-
-//     // Send token in response
-//     return res.json({ token, role: user.role });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
-// module.exports = {
-//   registerUser,
-//   login,
-// };
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const cookie=require('cookie')
 
 const jwtSecretKey = process.env.JWT_SECRET_KEY;
 const nodemailerTransporter = nodemailer.createTransport({
@@ -150,11 +68,13 @@ const login = async (req, res) => {
 
     // Generate JWT token
     const token = jwt.sign({ userId: user._id }, jwtSecretKey, {
-      expiresIn: "1month",
+      expiresIn: "1d",
     });
 
     // Send token in response
-    return res.json({ token });
+    // return res.json({ token });
+    res.cookie('token',token,{httpOnly:true,maxAge:360000})
+    return res.json({message:"login succesfully"})
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
@@ -162,37 +82,13 @@ const login = async (req, res) => {
 };
 
 const resetPassword = async (req, res) => {
-  try {
-    const { email } = req.body;
-
-    // Find the user by email
-    const user = await User.findOne({ email });
-
-    // If user is not found
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // Generate reset password token
-    const resetToken = jwt.sign({ userId: user._id }, jwtSecretKey, {
-      expiresIn: "1h",
-    });
-
-    // Send email with reset password link
-    const resetLink = `http://sarathlalsh@gmail.com/reset-password/${resetToken}`;
-    const mailOptions = {
-      from: "sarathlalsh@gmail.com",
-      to: user.email,
-      subject: "Reset Password",
-      text: `Please click on the following link to reset your password: ${resetLink}`,
-    };
-    await nodemailerTransporter.sendMail(mailOptions);
-
-    return res.json({ message: "Reset password link sent to your email" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
+  // const {email}=req.body;
+  // try{
+  //   const user=await User.findOne({email})
+  //   if(!user){
+  //     return res.status
+  //   }
+  // }
 };
 
 const setPassword = async (req, res) => {
